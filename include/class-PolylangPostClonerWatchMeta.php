fc2c16ed698b8075b076d9c9078da1adfa761039
mcguffin <joern@flyingletters.com>
2015-10-30 13:27:10 +0100

    Support fields inside repeaters

--- include/class-PolylangPostClonerWatchMeta.php
+++ include/class-PolylangPostClonerWatchMeta.php
@@ -13,6 +13,8 @@ class PolylangPostClonerWatchMeta {
 	 */
 	private $watch_meta_keys = array( '_thumbnail_id' );
 
+	private $watch_meta_keys_repeater = array( );
+
 	/**
 	 *	Holding the singleton instance
 	 */
@@ -56,12 +58,44 @@ class PolylangPostClonerWatchMeta {
 			// ... and add the relational ones to our watchlist.
 			foreach( $all_acf_fields as $field ) {
 				$field_settings = unserialize( $field->post_content );
-				if ( in_array($field_settings['type'] , $this->acf_types_to_watch ) )
-					$this->watch_meta_keys[] = $field->post_excerpt; // add meta key name
+				if ( in_array($field_settings['type'] , $this->acf_types_to_watch ) ) {
+					if ( $this->is_repeater_child( $field ) ) {
+						$meta_key = $this->get_repeater_meta_key_parts( $field );
+					} else {
+						$meta_key = $field->post_excerpt;
+					}
+					$this->watch_meta_keys[] = $meta_key; // add meta key name
+				}
 			}
 		}
 	}
 	
+	/**
+	 *	Return whether a field is part of a repeater field.
+	 *
+	 *	@param $field	object	ACF Field Post object
+	 *
+	 *	@return	bool
+	 */
+	function is_repeater_child( $field ) {
+		return get_post_type($field->post_parent) === 'acf-field';
+	}
+	
+	
+	/**
+	 *	Return meta key parts for an acf repeater child.
+	 *
+	 *	@param $field	object	ACF Field Post object which is part of a repeater (check with is_repeater_child first)
+	 *
+	 *	@return	array	holding the meta key parts
+	 */
+	function get_repeater_meta_key_parts( $field ) {
+		$meta_key = array( $field->post_excerpt );
+		while ( ($field = get_post($field->post_parent)) && $field->post_type == 'acf-field' ) {
+			$meta_key[] = $field->post_excerpt;
+		}
+		return array_reverse($meta_key);
+	}
 	
 	/**
 	 *	Handle meta keys of a translation group.
@@ -74,11 +108,26 @@ class PolylangPostClonerWatchMeta {
 	 *	@action 'pll_save_post'
 	 */
 	function handle_meta( $source_post_id , $source_post , $translation_group ) {
+		global $wpdb;
 		$watch_meta_keys = apply_filters( 'polylang_watch_meta_keys' , $this->watch_meta_keys );
 		$force_update = false;
 		foreach ( $translation_group as $lang => $new_post_id ) {
+			// prepare meta keys
+			$post_watch_meta_keys = array();
+			foreach( $watch_meta_keys as $meta_key ) {
+				if ( is_array($meta_key) ) {
+					$query = $wpdb->prepare("SELECT meta_key FROM $wpdb->postmeta WHERE meta_key REGEXP %s AND post_id=%d",
+							sprintf('^%s$',implode( '_[0-9]_' ,$meta_key ) ),
+							$new_post_id
+						);
+					$meta_keys = $wpdb->get_col($query);
+					$post_watch_meta_keys = array_merge($post_watch_meta_keys,$meta_keys);
+				} else {
+					$post_watch_meta_keys[] = $meta_key;
+				}
+			}
 			if ( $new_post_id != $source_post_id ) { // YES, we do this chack!
-				foreach ( $watch_meta_keys as $meta_key ) {
+				foreach ( $post_watch_meta_keys as $meta_key ) {
 					$old_meta_value = $meta_value = maybe_unserialize( get_post_meta( $new_post_id , $meta_key , true ) );
 					
 					if ( is_array( $meta_value ) ) {
